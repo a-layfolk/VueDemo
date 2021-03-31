4703d08c6a0e4a56595ed491f46bb96e178c5162
a-layfolk <1808150553@qq.com>
2021-03-31 22:31:02 +0800

    自己封装了一个响应式的 table 组件

---
+++ responsive-table-component/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <lay-table :layTheadData="layTheadData" :layTbodyData="layTbodyData"></lay-table>
+  </div>
+</template>
+
+<script>
+import LayTable from './components/layTable.vue'
+
+export default {
+  name: 'App',
+  components: {
+    LayTable
+  },
+  data() {
+    return {
+      layTheadData: ["表头 1", "表头 2", "表头 3", "表头 4"],
+      layTbodyData: [
+        ["表格 1-1", "表格 1-2", "表格 1-3", "表格 1-4"],
+        ["表格 2-1", "表格 2-2", "表格 2-3", "表格 2-4"],
+      ],
+    }
+  }
+};
+</script>
+
+<style></style>
---
+++ responsive-table-component/src/components/layTable.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="lay-table">
+    <table>
+      <thead>
+        <tr>
+          <th v-for="(thData, index) in layTheadData" :key="thData + index">
+            {{ thData }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(trData, index) in layTbodyData" :key="trData + index">
+          <td v-for="(tdData, index) in trData" :key="tdData + index" :data-label="layTheadData[index]">
+            {{ tdData }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LayTable",
+  props: {
+    layTheadData: Array,
+    layTbodyData: Array,
+  },
+  data() {
+    return {
+      isPhone: false,
+    };
+  },
+};
+</script>
+
+<style scoped>
+  table {
+    width: 100%;
+    /* 合并边框 */
+    border-collapse: collapse;
+  }
+  table tr {
+    border: 1px solid #ccc;
+  }
+  table th,
+  table td {
+    text-align: center;
+    padding: 10px;
+  }
+  table th {
+    font-size: 14px;
+    letter-spacing: 1px;
+  }
+
+  @media(max-width:768px) {
+    table thead {
+      display: none;
+    }
+    table tr {
+      display: block;
+      margin-bottom: 10px;
+    }
+    table td {
+      display: block;
+      text-align: right;
+    }
+    table td:before {
+      float: left;
+      content: attr(data-label);
+      font-weight: bold;
+    }
+  }
+</style>
